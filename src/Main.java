b78e702f209328ae34eb48fe195d874a6630222a
espertusnu <espertusnu@users.noreply.github.com>
2025-04-09 20:29:09 -0700

    Update quotes for 2025

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
                 new BulletList(List.of(
                         "There are only two hard things in computer science: " +
                                 "cache invalidation, naming, and off-by-one errors. --Jeff Atwood",
-                        "AI is whatever hasn't been done yet. --Larry Tesler",
+                        "Program testing can be used to show the presence of bugs, but never to show their absence! " +
+                                "-- Edsger Dijkstra",
                         "I'm not a great programmer. I'm just a good programmer with great habits. --Kent Beck"
                 )));
         HtmlGeneratingVisitor visitor = new HtmlGeneratingVisitor();
